37a8732c732f06aa4290254833517c855c4f8bbe
Marcos Duarte <Marcos.duarte1501@gmail.com>
2025-11-17 17:37:18 -0300

    Acessando Site de vagas

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ URL= os.getenv("URL_Site")
 print (f"URL DO SITE {URL}")
 
 #entrar no site
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get('https://ssi.aexecutiva.com.br/vagas')
+input('')
