64272b1539432be0f20055da60e15e59daddce96
Marcos Duarte <Marcos.duarte1501@gmail.com>
2025-11-18 07:12:51 -0300

    Criando For e Try para cidades

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+#importações 
 import os
 from dotenv import load_dotenv
+from selenium import webdriver
+from selenium .webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from time import sleep
 
-load_dotenv()
 
-URL= os.getenv("URL_Site")
+#definir o que fazer
+#entrar no site - ok
+#acessar cidade desejada (americana, nova odessa, santa barbada do oeste)-
+#pegar jobs da cidade desejada
+#printar no arquivo csv
+#ir para a proxima cidade - loop
 
 
-print (f"URL DO SITE {URL}")
 
 #entrar no site
-from selenium import webdriver
-
-
 driver = webdriver.Chrome()
 driver.get('https://ssi.aexecutiva.com.br/vagas')
-input('')
+sleep(6)
+
+cidades_busca = [
+    '//*[@id="cidade"]/option[2]' , #americana
+    '//*[@id="cidade"]/option[8]', #nova odessa
+    '//*[@id="cidade"]/option[10]', #sta brb doeste
+]
+
+for nome_da_cidade in cidades_busca:
+    print(f'Cidades sendo buscada, calma um pouco :D')
+    print(f"Buscando vagas para a cidade {nome_da_cidade}")
+
+
+    try:
+        selecionar_cidade =  driver.find_element(By.XPATH,'//*[@id="cidade"]')
+        selecionar_cidade.click()
+        sleep(5)
+    except:
+        print('Não foi possivel localizar a cidade')
