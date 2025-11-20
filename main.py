604683353872f99a8f8a35cd5f681bd9e4e092cf
Marcos Duarte <Marcos.duarte1501@gmail.com>
2025-11-20 08:56:04 -0300

    Adicionando Logica

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ dados = {
     'Salário': [],
     'Descrição': []
 }
+linhas = driver.find_elements('css selector', '#tbl-vagas > tbody > tr')
+
+for linha in linhas:
+    colunas = linha.find_elements('tag name', 'td')
+    
+    if len(colunas) >= 5:
+        dados['Ref'].append(colunas[1].text.strip())
+        dados['Cargo'].append(colunas[2].text.strip())
+        dados['Local'].append(colunas[3].text.strip())
+        dados['Salário'].append(colunas[4].text.strip())
+        dados['Descrição'].append(colunas[5].text.strip())
+
+driver.quit()
+
